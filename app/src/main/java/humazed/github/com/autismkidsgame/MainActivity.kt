bbc2477c31c599a7feea4ed920783b8a2e11783d
humazed <humazed@gmail.com>
2018-03-28 23:42:26 +0200

    add fake chooser for MatchingActivity

--- app/src/main/java/humazed/github/com/autismkidsgame/MainActivity.kt
+++ app/src/main/java/humazed/github/com/autismkidsgame/MainActivity.kt
@@ -2,6 +2,7 @@ package humazed.github.com.autismkidsgame
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.afollestad.materialdialogs.MaterialDialog
 import humazed.github.com.autismkidsgame.draw.DrawActivity
 import humazed.github.com.autismkidsgame.matching.MatchingActivity
 import humazed.github.com.autismkidsgame.puzzle.ui.ChoosePicActivity
@@ -18,9 +19,21 @@ class MainActivity : AppCompatActivity() {
             ChoosePicActivity.actionStart(this)
         }
 
+        fitTheShapeImageView.setOnClickListener {
+            MaterialDialog.Builder(this)
+                    .title(R.string.level_difficulty)
+                    .items("Easy", "Medium", "Hard")
+                    .itemsCallbackSingleChoice(0) { dialog, view, which, text ->
+                        startActivity<MatchingActivity>()
+                        true
+                    }
+                    .positiveText(R.string.action_ok)
+                    .negativeText(R.string.action_cancel)
+                    .show()
+        }
 
         findTheDiffImageView.setOnClickListener {
-            startActivity<MatchingActivity>()
+            //            startActivity<MatchingActivity>()
         }
 
         drawImageView.setOnClickListener {
